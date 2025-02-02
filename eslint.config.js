ea97f923ffd2f14b3413651de36013c22a37e677
mkotelnikov <maksim.kotelnikov@soft-control.ru>
2025-02-02 15:37:01 +0500

    add actions

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,11 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ]
 
 // module.exports = {
